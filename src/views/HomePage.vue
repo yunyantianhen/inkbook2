b0f9e6292086241cbe4ff43f2a25af8b367bb31f
hty <2940345812@11.com>
2022-08-04 10:20:58 +0800

    cookie

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -144,6 +144,36 @@
 <script>
 import qs from "qs";
 
+/**
+ * 设置cookie
+ */
+export function setCookie(name, value, hours = 24){
+  let str = name + "=" + value;
+  const time = new Date(new Date().getTime() + hours * 3600 * 1000).toGMTString();  // toGMTstring将时间转换成字符串
+  str += "; expires=" + time;
+  // 写入Cookie
+  document.cookie = str;
+}
+
+/**
+ * 获取cookie
+ */
+export function getCookie(name){
+  const reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)"),
+      arr = document.cookie.match(reg);
+  if (arr !== null) {
+    return arr[2];
+  } else {
+    return null;
+  }
+}
+
+/**
+ * 清除cookie
+ */
+export function clearCookie(name){
+  setCookie(name, '', -1)
+}
 export default {
   name: "HomePage",
   data() {
