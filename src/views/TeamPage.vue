9f1e639a6fdecc2c6a37ad0c2c932149fe760223
unknown <809816252@qq.com>
2022-08-04 17:22:55 +0800

    团队管理改动4

--- src/views/TeamPage.vue
+++ src/views/TeamPage.vue
@@ -9,6 +9,19 @@
 <div style="width: 800px; margin: auto">
   <div style="width: 800px; text-align: left">
     人员组成
+    <el-button style="position: relative; left: 630px" type="primary" @click="invite_user = true" round plain>邀请</el-button>
+    <el-dialog title="邀请成员" :visible.sync="invite_user" width="500px" @close="inviting">
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="110px">
+        <el-form-item label="被邀请人邮箱" prop="name">
+          <el-input v-model="addForm.mail"></el-input>
+        </el-form-item>
+      </el-form>
+      <!--底部区域-->
+      <span slot="footer" class="dialog-footer">
+            <el-button @click="invite_user = false">取 消</el-button>
+            <el-button type="primary" @click="addUser">确 定</el-button>
+          </span>
+    </el-dialog>>
     <br/>
     <br/>
   </div>
@@ -19,10 +32,30 @@
     <div style="height: 40px">成员4</div>
   </div>
   <div style="width: 400px; float: left">
-    <div style="height: 40px; font-weight: bold">创建者</div>
-    <div style="height: 40px; font-weight: bold">管理员</div>
-    <div style="height: 40px; font-weight: bold">成员</div>
-    <div style="height: 40px; font-weight: bold">成员</div>
+    <div style="height: 40px; width: 300px; font-weight: bold; float: left">创建者</div>
+    <div style="width: 100px; float: left">
+      <i class="el-icon-upload2" @click="BecomeAdministrator"></i>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <i class="el-icon-delete" @click="leave"></i>
+    </div>
+    <div style="height: 40px; width: 300px; font-weight: bold; float: left">管理员</div>
+    <div style="width: 100px; float: left">
+      <i class="el-icon-upload2" @click="BecomeAdministrator"></i>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <i class="el-icon-delete" @click="leave"></i>
+    </div>
+    <div style="height: 40px; width: 300px; font-weight: bold; float: left">成员</div>
+    <div style="width: 100px; float: left">
+      <i class="el-icon-upload2" @click="BecomeAdministrator"></i>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <i class="el-icon-delete" @click="leave"></i>
+    </div>
+    <div style="height: 40px; width: 300px; font-weight: bold; float: left">成员</div>
+    <div style="width: 100px; float: left">
+      <i class="el-icon-upload2" @click="BecomeAdministrator"></i>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <i class="el-icon-delete" @click="leave"></i>
+    </div>
   </div>
   <div style="width: 800px; text-align: left">
     负责项目
@@ -57,13 +90,42 @@
 <script>
 export default {
   name: "TeamPage",
+  data() {
+    return {
+      invite_user: false,
+      addForm:{
+        name: '',
+        mail: '',
+      },
+      addFormRules:{
+        name:[
+          { required : true, message:'请输入被邀请人的邮箱' ,trigger:'blur'},
+          { min :1 ,max:15, message: '项目名的长度在1~15个字符之间', trigger: 'blur'}
+        ]
+      },
+      teamList:[],
+      projectList:[]
+    }
+  },
   methods: {
     toitem() {
       this.$router.push('/itempage');
     },
     back() {
       this.$router.push('/');
-    }
+    },
+    inviting() {
+
+    },
+    addUser(){
+
+    },//邀请成员
+    BecomeAdministrator(){
+
+    },//将成员设置为管理员
+    leave() {
+
+    },//删除成员
   }
 }
 </script>
